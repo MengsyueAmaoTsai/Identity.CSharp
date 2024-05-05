feb107ef9233afdafcc05202257f7c062a97d5ff
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-06 03:24:50 +0800

    add identity server

--- RichillCapital.Identity/IdentityExtensions.cs
+++ RichillCapital.Identity/IdentityExtensions.cs
@@ -1,3 +1,5 @@
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,10 +10,20 @@ public static class IdentityExtensions
 {
     public static IServiceCollection AddIdentityForIdentityWeb(this IServiceCollection services)
     {
+        services
+            .AddIdentityServer(options =>
+            {
+                options.UserInteraction.LoginUrl = "/users/login";
+            })
+            .AddInMemoryClients(InMemoryClients.CreateClients())
+            .AddInMemoryIdentityResources(InMemoryIdentityResources.CreateIdentityResources())
+            .AddInMemoryApiScopes(InMemoryApiScopes.CreateApiScopes())
+            .AddInMemoryApiResources(InMemoryApiResources.CreateApiResources());
+
         return services;
     }
 
-    public static IServiceCollection AddTraderStudioWebIdentity(this IServiceCollection services)
+    public static IServiceCollection AddIdentityForTraderStudioWeb(this IServiceCollection services)
     {
         services
             .AddAuthentication(options =>
@@ -23,7 +35,7 @@ public static class IdentityExtensions
             .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
             {
                 options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.Authority = "http://127.0.0.1:9999";
+                options.Authority = "http://localhost:9999";
                 options.ClientId = "RichillCapital.TraderStudio.Web";
                 options.ClientSecret = "secret";
                 options.ResponseType = "code";
@@ -33,10 +45,81 @@ public static class IdentityExtensions
                 options.Scope.Add("offline_access");
                 options.SaveTokens = true;
                 options.GetClaimsFromUserInfoEndpoint = true;
-                options.TokenValidationParameters.NameClaimType = "name";
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role",
+                };
                 options.RequireHttpsMetadata = false;
             });
 
         return services;
     }
+}
+
+internal static class InMemoryApiResources
+{
+    internal static IEnumerable<ApiResource> CreateApiResources()
+    {
+        yield return new ApiResource("RichillCapital.Api", "Richill Capital API")
+        {
+            Scopes = { "RichillCapital.Api" },
+        };
+    }
+}
+
+internal static class InMemoryIdentityResources
+{
+    internal static IEnumerable<IdentityResource> CreateIdentityResources()
+    {
+        yield return new IdentityResources.OpenId();
+        yield return new IdentityResources.Profile();
+    }
+}
+
+internal static class InMemoryApiScopes
+{
+    internal static IEnumerable<ApiScope> CreateApiScopes()
+    {
+        yield return new ApiScope("RichillCapital.Api", "Richill Capital API");
+    }
+}
+
+internal static class InMemoryClients
+{
+    internal static IEnumerable<Client> CreateClients()
+    {
+        yield return WebApplications.TraderStudio;
+    }
+}
+
+internal static class WebApplications
+{
+    internal static readonly Client TraderStudio = new()
+    {
+        ClientId = "RichillCapital.TraderStudio.Web",
+        ClientName = "Trader Studio Web Client",
+        ClientSecrets =
+        {
+            new Secret("secret".Sha256()),
+        },
+        AllowedGrantTypes = GrantTypes.Code,
+        RequirePkce = true,
+        RedirectUris =
+        {
+            "http://localhost:9998/signin-oidc",
+        },
+        PostLogoutRedirectUris =
+        {
+            "http://localhost:9998/signout-callback-oidc",
+        },
+        AllowedScopes =
+        {
+            IdentityServerConstants.StandardScopes.OpenId,
+            IdentityServerConstants.StandardScopes.Profile,
+            "RichillCapital.Api",
+        },
+        AllowOfflineAccess = true,
+        RequireConsent = true,
+    };
 }
